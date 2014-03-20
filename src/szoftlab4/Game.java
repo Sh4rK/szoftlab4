f7aee0c55ec2cd9b364d605ee63252f65eec1d95
TALLÉR Bátor <tallerbator@gmail.com>
2014-03-20 20:33:17 +0100

    Added menu

--- src/szoftlab4/Game.java
+++ src/szoftlab4/Game.java
@@ -1,5 +1,10 @@
 package szoftlab4;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Game {
 	
 	/** 
@@ -7,6 +12,20 @@ public class Game {
 	 * A printEnter és a printExit metódusok ez alapján indentálnak
 	 **/
 	private static int tabs = 0;
+	
+	private Map map = new Map();
+	private Mission mission = new Mission();
+	private List<Enemy> enemies = new ArrayList<Enemy>();
+	private List<Projectile> projectiles = new ArrayList<Projectile>();
+	private List<Tower> towers = new ArrayList<Tower>();
+	private static Scanner sc = new Scanner(System.in);
+	
+	public Game(){
+		enemies.add(new Enemy());
+		projectiles.add(new Projectile());
+		towers.add(new Tower());
+	}
+	
 	public static void main(String[] args) {
 		Game game = new Game();
 		game.run();
@@ -60,18 +79,125 @@ public class Game {
 		--tabs;
 	}
 	
-	public void run(){
-		printEnter(this);
+	public static void printMessage(String msg){
+		String tab = "";
 		
-		run2(5);
-		
-		printExit(this);
+		for(int i = 0; i < tabs; ++i)
+			tab += "\t";
+		System.out.println(tab + "  " + msg);
+	}
+	
+	public static String printQuestion(String msg){
+		printMessage(msg);
+		String rtn = sc.next();
+		return rtn;
 	}
 
-	public void run2(int some_param){
-		printEnter(this, "some_param");
+	private void printMenu(){
+		printMessage("1. Torony építés");
+		printMessage("2. Akadály építés");
+		printMessage("3. Következő ellenség lekérése");
+		printMessage("4. Ellenség mozgatás");
+		printMessage("5. Torony tüzelés");
+		printMessage("6. Lövedék mozgatása");
+		printMessage("7. Varázskő felrakása");
+		printMessage("8. Kilépés");
+	}
+	
+	private int menu() throws IOException {
+		while(true){
+			String r = printQuestion("Kiírjam a menüt? I/N");
+			
+			if(r.equals("i") || r.equals("I"))
+				printMenu();
+			
+			int sel = Integer.parseInt(sc.next());
+			if(sel >= 1 && sel <= 8)
+				return sel;
+			else
+				printMessage("Ilyen menüpont nincs!");
+		}
+	}
+	
+	public void run(){
+		printEnter(this);
+		boolean exit = false;
+		while(!exit){
+			try {
+				int sel = menu();
+				switch(sel){
+				case 1:
+					buildTower(new Vector());
+					break;
+				case 2:
+					buildObstacle(new Vector());
+					break;
+				case 3:
+					mission.getNextEnemy();
+					break;
+				case 4:
+					enemies.get(0).move();
+					break;
+				case 5:
+					towers.get(0).attack(enemies);
+					break;
+				case 6:
+					projectiles.get(0).step();
+					break;
+				case 7:
+					addGem(new Vector(), new ObstacleGem());
+					break;
+				case 8:
+					exit = true;
+					break;
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			
+		}
 		
 		printExit(this);
 	}
+	
+	public void addGem(Vector v, TowerGem gem){
+		printEnter(this, "vector", "gem");
+		
+		printExit(this);
+	}
+	
+	public void addGem(Vector v, ObstacleGem gem){
+		printEnter(this, "vector", "gem");
+		
+		printExit(this);
+	}
+	
+	public void buildObstacle(Vector v){
+		printEnter(this, "vector");
+		
+		printExit(this);
+	}
+	
+	public void buildTower(Vector v){
+		printEnter(this, "vector");
+		
+		printExit(this);
+	}
+	
+	public boolean collidesWithObstacle(Vector v){
+		printEnter(this, "vector");
+		
+		printExit(this);
+		return true;
+	}
+	
+	public boolean collidesWithTower(Vector v){
+		printEnter(this, "vector");
+		
+		printExit(this);
+		return true;
+	}
+	
+	
 
 }
