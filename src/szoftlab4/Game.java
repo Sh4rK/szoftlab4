0fd6a88188ee0dc4e0faf7ce4c86866247c0e23f
Antal Szabó <szabo.antal.92@gmail.com>
2014-03-21 00:54:44 +0100

    Reformat

--- src/szoftlab4/Game.java
+++ src/szoftlab4/Game.java
@@ -6,94 +6,95 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Game {
-	
-	/** 
+
+	/**
 	 * Ez számolja milyen mélyen vagyunk a metódushívásokban
 	 * A printEnter és a printExit metódusok ez alapján indentálnak
-	 **/
+	 */
 	private static int tabs = 0;
-	
+	private static Scanner sc = new Scanner(System.in);
 	private Map map = new Map();
 	private Mission mission = new Mission();
 	private List<Enemy> enemies = new ArrayList<Enemy>();
 	private List<Projectile> projectiles = new ArrayList<Projectile>();
 	private List<Tower> towers = new ArrayList<Tower>();
-	private static Scanner sc = new Scanner(System.in);
-	
-	public Game(){
+
+	public Game() {
 		enemies.add(new Enemy());
 		projectiles.add(new Projectile());
 		towers.add(new Tower());
 	}
-	
+
 	public static void main(String[] args) {
 		Game game = new Game();
 		game.run();
 	}
-	
+
 	/**
 	 * Minden metódus hívás legelején ezt a metódust hívjuk meg.
 	 * A következőt írja ki a kimenetre: ->[:Osztály].metódus(param):
+	 *
 	 * @param sender az objektum, amiből meg lett hívva a metódus
-	 * @param params ha hívó metódusnak voltak paraméterei, akkor string formátumban meg kell adni a neveiket 
-	 **/
-	public static void printEnter(Object sender, String... params){
+	 * @param params ha hívó metódusnak voltak paraméterei, akkor string formátumban meg kell adni a neveiket
+	 */
+	public static void printEnter(Object sender, String... params) {
 		++tabs;
 		String methodEnter = "";
-		
-		for(int i = 0; i < tabs; ++i)
+
+		for (int i = 0; i < tabs; ++i)
 			methodEnter += "\t";
-		
+
 		Exception e = new Exception();
 		e.fillInStackTrace();
 		methodEnter += "->[:" + sender.getClass().getSimpleName() + "]";//->[:Game].buildTower(pos):
 		String methodName = e.getStackTrace()[1].getMethodName();
 		methodEnter += "." + methodName + "(";
-		if(params.length != 0){
+		if (params.length != 0) {
 			int i;
-			for(i = 0; i < params.length - 1; ++i)
+			for (i = 0; i < params.length - 1; ++i)
 				methodEnter += params[i] + ", ";
 			methodEnter += params[i];
 		}
 		methodEnter += "):";
 		System.out.println(methodEnter);
 	}
-	
+
 	/**
 	 * Minden metódus hívás legvégén ezt a metódust hívjuk meg.
 	 * A következőt írja ki a kimenetre: <-[:Osztály].metódus()
-	 * @param sender az objektum, amiből meg lett hívva a metódus 
-	 **/
-	public static void printExit(Object sender){
+	 *
+	 * @param sender az objektum, amiből meg lett hívva a metódus
+	 */
+	public static void printExit(Object sender) {
 		String methodExit = "";
-		
-		for(int i = 0; i < tabs; ++i)
+
+		for (int i = 0; i < tabs; ++i)
 			methodExit += "\t";
 		Exception e = new Exception();
 		e.fillInStackTrace();
 		methodExit += "<-[:" + sender.getClass().getSimpleName() + "]";//->[:Game].buildTower(pos):
 		String methodName = e.getStackTrace()[1].getMethodName();
 		methodExit += "." + methodName + "(" + ")";
-		
+
 		System.out.println(methodExit);
 		--tabs;
 	}
-	
-	public static void printMessage(String msg){
+
+	public static void printMessage(String msg) {
 		String tab = "";
-		
-		for(int i = 0; i < tabs; ++i)
+
+		for (int i = 0; i < tabs; ++i)
 			tab += "\t";
 		System.out.println(tab + "  " + msg);
 	}
-	
-	public static String printQuestion(String msg){
+
+	public static String printQuestion(String msg) {
 		printMessage(msg);
 		String rtn = sc.next();
 		return rtn;
 	}
 
-	private void printMenu(){
+	private void printMenu() {
 		printMessage("1. Torony építés");
 		printMessage("2. Akadály építés");
 		printMessage("3. Következő ellenség lekérése");
@@ -103,101 +104,100 @@ public class Game {
 		printMessage("7. Varázskő felrakása");
 		printMessage("8. Kilépés");
 	}
-	
+
 	private int menu() throws IOException {
-		while(true){
+		while (true) {
 			String r = printQuestion("Kiírjam a menüt? I/N");
-			
-			if(r.equals("i") || r.equals("I"))
+
+			if (r.equals("i") || r.equals("I"))
 				printMenu();
-			
+
 			int sel = Integer.parseInt(sc.next());
-			if(sel >= 1 && sel <= 8)
+			if (sel >= 1 && sel <= 8)
 				return sel;
 			else
 				printMessage("Ilyen menüpont nincs!");
 		}
 	}
-	
-	public void run(){
+
+	public void run() {
 		printEnter(this);
 		boolean exit = false;
-		while(!exit){
+		while (!exit) {
 			try {
 				int sel = menu();
-				switch(sel){
-				case 1:
-					buildTower(new Vector());
-					break;
-				case 2:
-					buildObstacle(new Vector());
-					break;
-				case 3:
-					mission.getNextEnemy();
-					break;
-				case 4:
-					enemies.get(0).move();
-					break;
-				case 5:
-					towers.get(0).attack(enemies);
-					break;
-				case 6:
-					projectiles.get(0).step();
-					break;
-				case 7:
-					addGem(new Vector(), new ObstacleGem());
-					break;
-				case 8:
-					exit = true;
-					break;
+				switch (sel) {
+					case 1:
+						buildTower(new Vector());
+						break;
+					case 2:
+						buildObstacle(new Vector());
+						break;
+					case 3:
+						mission.getNextEnemy();
+						break;
+					case 4:
+						enemies.get(0).move();
+						break;
+					case 5:
+						towers.get(0).attack(enemies);
+						break;
+					case 6:
+						projectiles.get(0).step();
+						break;
+					case 7:
+						addGem(new Vector(), new ObstacleGem());
+						break;
+					case 8:
+						exit = true;
+						break;
 				}
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
-			
+
 		}
-		
+
 		printExit(this);
 	}
-	
-	public void addGem(Vector v, TowerGem gem){
+
+	public void addGem(Vector v, TowerGem gem) {
 		printEnter(this, "vector", "gem");
-		
+
 		printExit(this);
 	}
-	
-	public void addGem(Vector v, ObstacleGem gem){
+
+	public void addGem(Vector v, ObstacleGem gem) {
 		printEnter(this, "vector", "gem");
-		
+
 		printExit(this);
 	}
-	
-	public void buildObstacle(Vector v){
+
+	public void buildObstacle(Vector v) {
 		printEnter(this, "vector");
-		
+
 		printExit(this);
 	}
-	
-	public void buildTower(Vector v){
+
+	public void buildTower(Vector v) {
 		printEnter(this, "vector");
-		
+
 		printExit(this);
 	}
-	
-	public boolean collidesWithObstacle(Vector v){
+
+	public boolean collidesWithObstacle(Vector v) {
 		printEnter(this, "vector");
-		
+
 		printExit(this);
 		return true;
 	}
-	
-	public boolean collidesWithTower(Vector v){
+
+	public boolean collidesWithTower(Vector v) {
 		printEnter(this, "vector");
-		
+
 		printExit(this);
 		return true;
 	}
-	
-	
+
 
 }
