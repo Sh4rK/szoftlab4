ac23a73154d79d24a39e7203de96378a123ff0ef
Nusee <nusee0726@hotmail.com>
2014-03-21 18:26:14 +0100

    Completed Map

--- src/szoftlab4/Map.java
+++ src/szoftlab4/Map.java
@@ -1,5 +1,32 @@
 package szoftlab4;
 
 public class Map {
-
+	
+	/**
+	 * @return Lehet-e a vector helyére Obstacle-t építeni.
+	 */
+	public boolean canBuildObstacle(Vector v){
+		
+		boolean b;
+		
+		printEnter(this, "vector");
+		b = printYesNoQuestion("Lehet építeni akadályt?");
+		
+		printExit(this);
+		return b;
+		
+	}
+	/**
+	 * @return Lehet-e a vector helyére Tower-t építeni.
+	 */
+	public boolean canBuildTower(Vector v){
+		
+		boolean b;
+		
+		printEnter(this, "vector");
+		b = printYesNoQuestion("Lehet építeni akadályt?");
+		
+		printExit(this);
+		return b;
+	}
 }
