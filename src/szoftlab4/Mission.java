d2c6e30440d4396d4b6687793cd1cce1e126f7b7
Nusee <nusee0726@hotmail.com>
2014-03-21 18:47:59 +0100

    Completed Mission and TowerGem

--- src/szoftlab4/Mission.java
+++ src/szoftlab4/Mission.java
@@ -1,8 +1,16 @@
 package szoftlab4;
 
 public class Mission {
+	
+	/**
+	 * 
+	 * @return Visszatér a listában tárolt következõ ellenséggel.
+	 */
+	public Enemy getNextEnemy() {
+		printEnter(this);
 
-	public void getNextEnemy() {
+		printExit(this);
+		return null;
 	}
 
 }
--- src/szoftlab4/TowerGem.java
+++ src/szoftlab4/TowerGem.java
@@ -1,5 +1,24 @@
 package szoftlab4;
 
-public class TowerGem {
+public class TowerGem extends Gem{
+	/**
+	 * 
+	 * @return Visszatér a támadás szorzójával.
+	 */
+	public double getDamageMultiplier(){
+		printEnter(this);
 
+		printExit(this);
+		return 0;
+	}
+	/**
+	 * 
+	 * @return Visszatér a támadási távolság szorzójával.
+	 */
+	public double getRateMultiplier(){
+		printEnter(this);
+
+		printExit(this);
+		return 0;
+	}
 }
