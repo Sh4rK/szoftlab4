ffdcdd9e475b7adeb90229ee91c564c6f108b70b
Antal Szabó <szabo.antal.92@gmail.com>
2014-03-23 16:44:31 +0100

    Fixed typo and changed a few things in Map

--- src/szoftlab4/Map.java
+++ src/szoftlab4/Map.java
@@ -14,39 +14,35 @@ public class Map {
 	/**
 	 * A kapott útvonalról betölti a Map-et;
 	 */
-	public Map(String str){
-		printEnter(this);
+	public Map(String path){
+		printEnter(this, "path");
+
 		wp = new Waypoint(new Vector());
+
 		printExit(this);
 	}
 	
 	/**
-	 * @return Lehet-e a vector helyére Obstacle-t építeni.
+	 * @return Lehet-e az adott helyre Obstacle-t építeni.
 	 */
-	public boolean canBuildObstacle(Vector v){
-		
-		boolean b;
-		
-		printEnter(this, "vector");
+	public boolean canBuildObstacle(Vector position){
+		printEnter(this, "position");
+
 		wp.getPosition();
-		
-		b = printYesNoQuestion("Lehet építeni akadályt?");
+		boolean b = printYesNoQuestion("Lehet építeni akadályt?");
 		
 		printExit(this);
 		return b;
-		
 	}
+
 	/**
 	 * @return Lehet-e a vector helyére Tower-t építeni.
 	 */
-	public boolean canBuildTower(Vector v){
-		
-		boolean b;
-		
-		printEnter(this, "vector");
+	public boolean canBuildTower(Vector position){
+		printEnter(this, "position");
+
 		wp.getPosition();
-		
-		b = printYesNoQuestion("Lehet építeni akadályt?");
+		boolean b = printYesNoQuestion("Lehet építeni tornyot?");
 		
 		printExit(this);
 		return b;
