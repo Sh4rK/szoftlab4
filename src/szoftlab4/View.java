5e237f4bda55058a1bd21ab684734e554053437d
TALLÉR Bátor <tallerbator@gmail.com>
2014-05-03 14:31:02 +0200

    Added View

---
+++ src/szoftlab4/View.java
@@ -0,0 +1,40 @@
+package szoftlab4;
+
+public class View {
+
+	public void enemyAdded(Enemy en) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void projectileExploded(Projectile p) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void projectileAdded(Projectile p) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void enemyDied(Enemy en) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void towerAdded(Tower t) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void obstacleAdded(Obstacle o) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void drawAll() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
