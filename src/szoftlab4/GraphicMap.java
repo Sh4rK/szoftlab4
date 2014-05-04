ce996d000acb1893947a1b232e1200b7c8e2b3f4
TÖRÖK Attila <torokati44@gmail.com>
2014-05-05 01:38:20 +0200

    Fixed the drawing of GraphicMap.

--- src/szoftlab4/GraphicMap.java
+++ src/szoftlab4/GraphicMap.java
@@ -28,6 +28,7 @@ public class GraphicMap extends Drawable {
 	@Override
 	public void draw(Graphics g) {
 		Graphics2D g2 = (Graphics2D)g;
+		AffineTransform originalTransform = g2.getTransform();
 		
 		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
 				RenderingHints.VALUE_ANTIALIAS_ON);
@@ -35,15 +36,20 @@ public class GraphicMap extends Drawable {
 				RenderingHints.VALUE_INTERPOLATION_BICUBIC);
 		
 		g2.drawImage(img, 0, 0, null);
+		
+		g2.setColor(new Color(0x552200));
+		
 		for (Waypoint wp : m.getWaypoints()) {
-			g2.setTransform(new AffineTransform());
+			g2.setTransform(originalTransform);
+			
 			Vector pos = Game.toMouseCoords(wp.getPosition());
 			Vector size = Game.toMouseCoords(new Vector(4, 4)); // TODO
-			g2.setColor(new Color(0x552200));
+			
 			g2.fillOval((int)pos.x-(int)(size.x/2), (int)pos.y-(int)(size.y/2), (int)size.x, (int)size.y);
+			
 			for (Waypoint w: wp.listNextWaypoints()) {
-				g2.setTransform(new AffineTransform());
-				g2.setColor(new Color(0x552200));
+				g2.setTransform(originalTransform);
+				
 				Vector wpos = Game.toMouseCoords(w.getPosition());
 				g2.rotate(Math.atan2(wpos.y - pos.y, wpos.x - pos.x), pos.x, pos.y);
 				g2.fillRect((int)pos.x, (int)pos.y-(int)(size.y/2), (int)(pos.getDistance(wpos)), (int)size.y);
